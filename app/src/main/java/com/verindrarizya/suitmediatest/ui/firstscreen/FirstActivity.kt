627162b92d37c331a0de05f261e7091fc1b12651
verindrarizya <v.rizya22@gmail.com>
2022-01-14 14:28:14 +0700

    Initial commit & adding dependencies

---
+++ app/src/main/java/com/verindrarizya/suitmediatest/ui/firstscreen/FirstActivity.kt
@@ -0,0 +1,12 @@
+package com.verindrarizya.suitmediatest.ui.firstscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.verindrarizya.suitmediatest.R
+
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+    }
+}
